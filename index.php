98560db218765bbce9c396576dcffda88e724152
joel2035 <joel.pokam@hetic.net>
2019-02-25 14:33:54 +0100

    second commit

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
- <?php
-
-  ?>
-<!DOCTYPE html>
-
-<html lang="en" dir="ltr">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title>one plus 7</title>
-  <title></title>
-</head>
-
-<body>
-  <h1>Super site pour le one plus 7</h1>
-</body>
-
-</html>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" type="text/css" media="screen" href="Assets/Styles/main.css">
+   <title>TOTEM COFFEEEE @ STORE</title>
+ </head>
+ <body>
+   <header class="header">
+   <div class="contentHeader">
+   
+   </div>
+   </header>
+   <div class="container">
+   
+   </div>
+ </body>
+ </html>
