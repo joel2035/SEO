e17417e55c7a1237c9a00aa52b133101a3f6eb98
joel2035 <joel.pokam@hetic.net>
2019-02-25 14:16:43 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+ <?php
+
+  ?>
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>one plus 7</title>
+  <title></title>
+</head>
+
+<body>
+  <h1>Super site pour le one plus 7</h1>
+</body>
+
+</html>
